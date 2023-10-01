f3c3fe30955f7abe278b908fdc1adc39e3982ce4
masa <koedame032@gmail.com>
2023-10-01 15:08:52 +0900

    #2 ニコ生ランキングサイトから配信タイトルを取得

--- get_streaming_url.py
+++ get_streaming_url.py
@@ -8,12 +8,24 @@ url = "https://live.nicovideo.jp/ranking"
 response = requests.get(url)
 response.encoding = response.apparent_encoding  # エンコーディングを設定
 
+
+# print("--- response ---")
+# print(response)
+# print("--- response.encoding ---")
+# print(response.encoding)
+# print("--- response.text ---")
+# print(response.text)
+
 # BeautifulSoupオブジェクトを作成
 soup = BeautifulSoup(response.text, 'html.parser')
 
+# print("--- soup ---")
+# print(soup)
+
 # 配信タイトルが格納されている要素を探してリストに追加
 titles = []
-for title_div in soup.select('.Mg-btm0px.Pd-right10px.Pd-left10px.H-fix2col .Fz-m.Fw-b'):
+# for title_div in soup.select('.Mg-btm0px.Pd-right10px.Pd-left10px.H-fix2col .Fz-m.Fw-b'):
+for title_div in soup.select('.___rk-program-card-detail-title___gJhRF'):
     titles.append(title_div.text.strip())
 
 # 取得した配信タイトルを出力
--- memo.md
+++ memo.md
@@ -1,6 +1,15 @@
 # プロジェクト作成
 
-## ランキングからのデータ取得
+## ニコ生ランキングから配信ページの URL を取得
+
+### 配信ページ箇所のソースコード
+
+```
+<a class="___rk-program-card-detail-title___gJhRF" href="https://live.nicovideo.jp/watch/lv342949002?ref=RankingPage-UserProgramListSection-ProgramCard&amp;provider_type=community" title="家賃2万円の古民家！？内見しにきた！！"><span>家賃2万円の古民家！？内見しにきた！！</span></a>
+
+<a class="___rk-program-card-detail-title___gJhRF" href="https://live.nicovideo.jp/watch/lv342947517?ref=RankingPage-UserProgramListSection-ProgramCard&amp;provider_type=community" title="皆様ーこんにちはー何もする事がない！"><span>皆様ーこんにちはー何もする事がない！</span></a>
+
+```
 
 ### GPT への注文
 
